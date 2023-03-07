f80f96dee175c986e0a41a1f7ccfcc199b10b4e5
MaxAkonde <devdevmaster@gmail.com>
2023-03-07 10:06:52 +0100

    copywrite v 0.4

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -444,7 +444,7 @@
     <!-- Project End -->
 
 
-    {{-- <!-- Team Start -->
+    <!-- Team Start -->
     <div class="container-xxl py-5">
         <div class="container">
             <div class="text-center mx-auto wow fadeInUp" data-wow-delay="0.1s" style="max-width: 500px;">
@@ -455,53 +455,36 @@
                 <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
                     <div class="team-item rounded overflow-hidden pb-4">
                         <img class="img-fluid mb-4" src="{{ asset('user/img/team-1.jpg') }}" alt="">
-                        <h5>Alex Robin</h5>
-                        <span class="text-primary">Founder & CEO</span>
-                        <ul class="team-social">
-                            <li><a class="btn btn-square" href="#"><i class="fab fa-facebook-f"></i></a></li>
-                            <li><a class="btn btn-square" href="#"><i class="fab fa-twitter"></i></a></li>
-                            <li><a class="btn btn-square" href="#"><i class="fab fa-instagram"></i></a></li>
-                            <li><a class="btn btn-square" href="#"><i class="fab fa-linkedin-in"></i></a></li>
-                        </ul>
+                        <h5>Alex V.</h5>
+                        <span class="text-primary">Founder & CEO Performance Project manager</span>
                     </div>
                 </div>
                 <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
                     <div class="team-item rounded overflow-hidden pb-4">
                         <img class="img-fluid mb-4" src="{{ asset('user/img/team-2.jpg') }}" alt="">
-                        <h5>Adam Crew</h5>
-                        <span class="text-primary">Co Founder</span>
-                        <ul class="team-social">
-                            <li><a class="btn btn-square" href="#"><i class="fab fa-facebook-f"></i></a></li>
-                            <li><a class="btn btn-square" href="#"><i class="fab fa-twitter"></i></a></li>
-                            <li><a class="btn btn-square" href="#"><i class="fab fa-instagram"></i></a></li>
-                            <li><a class="btn btn-square" href="#"><i class="fab fa-linkedin-in"></i></a></li>
-                        </ul>
+                        <h5>Alicia H</h5>
+                        <span class="text-primary">Psychologist & Cognitive Expert marketing</span>
                     </div>
                 </div>
                 <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
                     <div class="team-item rounded overflow-hidden pb-4">
                         <img class="img-fluid mb-4" src="{{ asset('user/img/team-3.jpg') }}" alt="">
-                        <h5>Boris Johnson</h5>
-                        <span class="text-primary">Executive Manager</span>
-                        <ul class="team-social">
-                            <li><a class="btn btn-square" href="#"><i class="fab fa-facebook-f"></i></a></li>
-                            <li><a class="btn btn-square" href="#"><i class="fab fa-twitter"></i></a></li>
-                            <li><a class="btn btn-square" href="#"><i class="fab fa-instagram"></i></a></li>
-                            <li><a class="btn btn-square" href="#"><i class="fab fa-linkedin-in"></i></a></li>
-                        </ul>
+                        <h5>Gavin M.</h5>
+                        <span class="text-primary">Digital Marketer 360 Expert</span>
                     </div>
                 </div>
                 <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.7s">
                     <div class="team-item rounded overflow-hidden pb-4">
                         <img class="img-fluid mb-4" src="{{ asset('user/img/team-4.jpg') }}" alt="">
-                        <h5>Robert Jordan</h5>
-                        <span class="text-primary">Digital Marketer</span>
-                        <ul class="team-social">
-                            <li><a class="btn btn-square" href="#"><i class="fab fa-facebook-f"></i></a></li>
-                            <li><a class="btn btn-square" href="#"><i class="fab fa-twitter"></i></a></li>
-                            <li><a class="btn btn-square" href="#"><i class="fab fa-instagram"></i></a></li>
-                            <li><a class="btn btn-square" href="#"><i class="fab fa-linkedin-in"></i></a></li>
-                        </ul>
+                        <h5>Caleb W.</h5>
+                        <span class="text-primary">Paid ads specialist & data analyst</span>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.7s">
+                    <div class="team-item rounded overflow-hidden pb-4">
+                        <img class="img-fluid mb-4" src="{{ asset('user/img/team-4.jpg') }}" alt="">
+                        <h5>Morgan A.</h5>
+                        <span class="text-primary">Only Fan & Myms ecosystem & talent manager</span>
                     </div>
                 </div>
             </div>
@@ -510,7 +493,7 @@
     <!-- Team End -->
 
 
-    <!-- Testimonial Start -->
+    {{-- <!-- Testimonial Start -->
     <div class="container-xxl pt-5">
         <div class="container">
             <div class="text-center text-md-start pb-5 pb-md-0 wow fadeInUp" data-wow-delay="0.1s"
